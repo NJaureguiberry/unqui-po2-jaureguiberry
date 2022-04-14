a15497eafa8c0d21fd1a773e00a48dbe43dcd391
NicoJaure <NicoJaure@DESKTOP-M8VSMT1>
2022-04-14 13:36:16 -0300

    prueba

---
+++ test/ar/unq/po2/tp4/pruebaTest.java
@@ -0,0 +1,14 @@
+package ar.unq.po2.tp4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class pruebaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
